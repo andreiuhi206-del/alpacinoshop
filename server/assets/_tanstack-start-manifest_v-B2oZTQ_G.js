3c6ddbba5247571fa63843dbe0998df10bf4fbb0
andreiuhi206-del <andreiuhi206-del@users.noreply.github.com>
2026-04-29 17:06:56 +0000

    deploy: dbfe3936360c66b284de8451472de6e430a88f5d

---
+++ server/assets/_tanstack-start-manifest_v-B2oZTQ_G.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/home/runner/work/alpacinoshop/alpacinoshop/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-YsrUJ_jg.js"] }, "/": { "filePath": "/home/runner/work/alpacinoshop/alpacinoshop/src/routes/index.tsx", "preloads": ["/assets/index-CARhYqoj.js"] } }, "clientEntry": "/assets/main-YsrUJ_jg.js" });
+export {
+  tsrStartManifest
+};
